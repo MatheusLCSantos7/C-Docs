08a3e7ae30a0320757a26cf34e03dde8866f7071
Matheus LCSantos <matheuslcsantosbh@gmail.com>
2022-04-26 19:54:53 -0300

    Create TriangleAnalisator.cs

---
+++ TriangleAnalisator.cs
@@ -0,0 +1,34 @@
+using System;
+					
+public class Program
+{
+	public static void Main()
+	{
+		Console.WriteLine("ANALISADOR DE TRIÂNGULOS");
+		
+		int[] ladoTriangle = new int[3];
+		
+		Console.WriteLine($"Digite o valor do primeiro lado:  \n");
+		ladoTriangle[0] = int.Parse(Console.ReadLine());
+		
+		Console.WriteLine($"Digite o valor do segundo lado:  \n");
+		ladoTriangle[1] = int.Parse(Console.ReadLine());
+		
+		Console.WriteLine($"Digite o valor do terceiro lado:  \n");
+		ladoTriangle[2] = int.Parse(Console.ReadLine());
+		
+		if ((ladoTriangle[0] == ladoTriangle[1]) && ladoTriangle[1] == ladoTriangle[2] && ladoTriangle[0] == ladoTriangle[2] )
+            {
+                Console.WriteLine($"É um Triângulo Equilátero\n");
+            } else if ((ladoTriangle[0] != ladoTriangle[1]) && ladoTriangle[1] != ladoTriangle[2] && ladoTriangle[0] != ladoTriangle[2] )
+            {
+                Console.WriteLine($"É um Triângulo Escaleno\n"); 
+			}
+			else
+			{
+				 Console.WriteLine($"É um Triângulo Isóceles \n");
+				}
+			
+			
+	}
+}
